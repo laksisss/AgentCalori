cbe3fb0ddb700899517c8d705c90dfd0ca61f229
laksisss <43779451+laksisss@users.noreply.github.com>
2026-06-29 15:22:27 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,12 @@ load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+GIGACHAT_CREDENTIALS = os.getenv("GIGACHAT_CREDENTIALS")
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///./bot.db")
 
 APP_URL = os.getenv("APP_URL", "https://caloriv2.up.railway.app")
 
-FREE_DAILY_LIMIT = 10
+
 CACHE_TTL = 86400
 
 if DATABASE_URL.startswith("postgresql://"):
